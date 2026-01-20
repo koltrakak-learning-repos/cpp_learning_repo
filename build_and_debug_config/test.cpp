3ef1eed32d5e0ce8b44508eb83067241f99d4aca
KoolKevin <kevinkoltraka1011@gmail.com>
2026-01-20 18:46:49 +0100

    asd

---
+++ build_and_debug_config/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Enter an integer: ";
+
+    int num{};
+    std::cin >> num;
+
+    std::cout << "Double that number is: " <<  num * 2 << '\n'; // use an expression to multiply num * 2 at the point where we are going to print it
+
+    return 0;
+}
